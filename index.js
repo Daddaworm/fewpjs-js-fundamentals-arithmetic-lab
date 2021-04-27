b020447d4c8b6c4959ca95a870695c92ba42de0d
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-04-26 23:59:39 -0400

    commit tree eb564cf6176725951d057e087c290f07bffd11a0

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
-// Write your code here
+// create a variable called multiply and set to an equation that will multiply the variables num1 and num2. The result of the multiplication should be 62
+let multiply;
+let num1 = 31;
+let num2 = 2;
+multiply = num1 * num2;
+
+// create a variable called remainder that will generate a random interget greater than zero
+let random = Math.floor(Math.random() * 10) + 1;
+
+// Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4
+let mod;
+let num3 = 8
+let num4 = 2
+mod = num3 / num4;
+
+// Create a variable called max that finds the highest number in a set; the value returned should be 20.
+let max = Math.max (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
